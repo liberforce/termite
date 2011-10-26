778c1deef7c63a41c40f21f33ecdf86f555c7a00
Luis Menina <liberforce@freeside.fr>
2011-10-26 23:59:28 +0200

    Factorize out the direction chosing code.

--- termite.c
+++ termite.c
@@ -1,97 +1,26 @@
 #include <stdlib.h>    // for malloc, calloc, free
 #include <stdio.h>     // for stdout, stderr, fprintf
 #include <string.h>    // for memset, strcmp, strlen
+#include <assert.h>    // for assert
 
 #include "termite.h"
 #include "ant.h"
 
-// The following is an example program displaying a basic ai that
-// tries all possible directions and then moves in whichever one
-// is not blocked by water (% characters).
-//
-// To see all the information contained in struct game_state and
-// struct game_info check ants.h. There is also a distance function
-// provided in bot.c
-
-void termite_do_turn(struct game_state *Game, struct game_info *Info)
+void termite_do_turn (struct game_state *Game, struct game_info *Info)
 {
-
-	// defining things just so we can do less writing
-	// UP and DOWN move up and down rows while LEFT and RIGHT
-	// move side to side. The map is just one big array.
-
-#define UP -Info->cols
-#define DOWN Info->cols
-#define LEFT -1
-#define RIGHT 1
-
 	int i;
 
+	// Chose moves for each ant
 	for (i = 0; i < Game->my_count; ++i) 
 	{
 		// the location within the map array where our ant is currently
 		Ant *ant = &Game->my_ants[i];
-
-		int offset = ant_get_row (ant) * Info->cols + ant_get_col (ant);
-
-		// Now here is the tricky part. We have to account for
-		// the fact that the map wraps (when you go off one edge
-		// you end up on the side of the map opposite that edge).
-		// This is done by checking to see if we are on the last
-		// row or column and if the direction we are taking would
-		// take us off the side of the map.
-		//
-		// For example, you can see here the West direction checks
-		// to see if we are in the first column, in which case "West"
-		// is a character a full row minus one from our location.
-
-		char obj_north, obj_east, obj_south, obj_west;
-
-		if (ant_get_col (ant) != 0)
-			obj_west = Info->map[offset + LEFT];
-		else
-			obj_west = Info->map[offset + Info->cols - 1];
-
-		if (ant_get_col (ant) != Info->cols - 1)
-			obj_east = Info->map[offset + RIGHT];
-		else
-			obj_east = Info->map[offset - Info->cols - 1];
-
-		if (ant_get_row (ant) != 0)
-			obj_north = Info->map[offset + UP];
-		else
-			obj_north = Info->map[offset + (Info->rows - 1)*Info->cols];
-
-		if (ant_get_row (ant) != Info->rows - 1)
-			obj_south = Info->map[offset + DOWN];
-		else
-			obj_south = Info->map[offset - (Info->rows - 1)*Info->cols];
-
-		char dir = -1;
-
-		// cycle through the directions, pick one that works
-
-		if (obj_north != '%')
-			dir = 'N';
-		else if (obj_east != '%')
-			dir = 'E';
-		else if (obj_south != '%')
-			dir = 'S';
-		else if (obj_west != '%')
-			dir = 'W';
+		char dir = termite_choose_ant_direction (Game, Info, ant);
 
 		// Now we do our move
-
-		if (dir != -1)
+		if (dir != DIR_NONE)
 			termite_move_ant (Game, Info, ant, dir);
 	}
-
-	// There are many ways to make this program better.
-	// For starters, try to avoid collisions between your
-	// own ants and make a conscious effort to gather food
-	// instead of walking around at random.
-	//
-	// Good luck!
 }
 
 // sends a move to the tournament engine and keeps track of ants new location
@@ -133,7 +62,7 @@ void termite_move_ant (struct game_state* Game, struct game_info* Info, Ant* ant
 // initializes the game_info structure on the very first turn
 // function is not called after the game has started
 
-void termite_init_ants(char *data, struct game_info *game_info) 
+void termite_init_ants (char *data, struct game_info *game_info) 
 {
 	char *replace_data = data;
 
@@ -408,3 +337,69 @@ void termite_init_map(char *data, struct game_info *game_info)
 		data = tmp_ptr + 1;
 	}
 }
+
+char termite_choose_ant_direction (struct game_state *Game, 
+		struct game_info *Info, 
+		Ant *ant)
+{
+	assert (ant != NULL);
+	int offset = ant_get_row (ant) * Info->cols + ant_get_col (ant);
+
+	// defining things just so we can do less writing
+	// UP and DOWN move up and down rows while LEFT and RIGHT
+	// move side to side. The map is just one big array.
+
+#define UP -Info->cols
+#define DOWN Info->cols
+#define LEFT -1
+#define RIGHT 1
+
+	// Now here is the tricky part. We have to account for
+	// the fact that the map wraps (when you go off one edge
+	// you end up on the side of the map opposite that edge).
+	// This is done by checking to see if we are on the last
+	// row or column and if the direction we are taking would
+	// take us off the side of the map.
+	//
+	// For example, you can see here the West direction checks
+	// to see if we are in the first column, in which case "West"
+	// is a character a full row minus one from our location.
+
+	char obj_north, obj_east, obj_south, obj_west;
+
+	if (ant_get_col (ant) != 0)
+		obj_west = Info->map[offset + LEFT];
+	else
+		obj_west = Info->map[offset + Info->cols - 1];
+
+	if (ant_get_col (ant) != Info->cols - 1)
+		obj_east = Info->map[offset + RIGHT];
+	else
+		obj_east = Info->map[offset - Info->cols - 1];
+
+	if (ant_get_row (ant) != 0)
+		obj_north = Info->map[offset + UP];
+	else
+		obj_north = Info->map[offset + (Info->rows - 1)*Info->cols];
+
+	if (ant_get_row (ant) != Info->rows - 1)
+		obj_south = Info->map[offset + DOWN];
+	else
+		obj_south = Info->map[offset - (Info->rows - 1)*Info->cols];
+
+	char dir = DIR_NONE;
+
+	// cycle through the directions, pick one that works
+
+	if (obj_north != '%')
+		dir = DIR_NORTH;
+	else if (obj_east != '%')
+		dir = DIR_EAST;
+	else if (obj_south != '%')
+		dir = DIR_SOUTH;
+	else if (obj_west != '%')
+		dir = DIR_WEST;
+
+	return dir;
+}
+
--- termite.h
+++ termite.h
@@ -4,11 +4,32 @@
 #include "game.h"
 #include "ant.h"
 
-void termite_init_map(char *data, struct game_info *game_info);
-void termite_init_game (struct game_info *game_info, struct game_state *game_state);
-void termite_init_ants(char *data, struct game_info *game_info);
+// Possible directions
+#define DIR_NORTH 'N'
+#define DIR_EAST  'E'
+#define DIR_SOUTH 'S'
+#define DIR_WEST  'W'
+#define DIR_NONE  'X'
 
-void termite_move_ant (struct game_state* Game, struct game_info* Info, Ant* ant, char dir);
-void termite_do_turn (struct game_state *Game, struct game_info *Info);
+void termite_init_map (char *data, 
+		struct game_info *game_info);
+
+void termite_init_game (struct game_info *game_info, 
+		struct game_state *game_state);
+
+void termite_init_ants (char *data, 
+		struct game_info *game_info);
+
+void termite_do_turn (struct game_state *Game, 
+		struct game_info *Info);
+
+void termite_move_ant (struct game_state* Game, 
+		struct game_info* Info, 
+		Ant* ant, 
+		char dir);
+
+char termite_choose_ant_direction (struct game_state *Game, 
+		struct game_info *Info, 
+		Ant *ant);
 
 #endif // TERMITE_H
