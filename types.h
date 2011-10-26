1f45c55ddee8e0c5643afb2fe8558f1fc4da4880
Luis Menina <liberforce@freeside.fr>
2011-10-26 11:25:42 +0200

    Add base types (inspired by GLib).

---
+++ types.h
@@ -0,0 +1,21 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef char gchar;
+typedef unsigned char guchar;
+
+typedef char gint8;
+typedef unsigned char guint8;
+
+typedef int gint;
+typedef unsigned int guint;
+
+typedef int gint32;
+typedef unsigned int guint32;
+
+typedef int gboolean;
+
+#define TRUE  1
+#define FALSE 0
+
+#endif // TYPES_H
