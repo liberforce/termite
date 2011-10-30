a7642f9118198bbea7fb0db79317956b21ceb499
Luis Menina <liberforce@freeside.fr>
2011-10-30 17:40:32 +0100

    Add g_debug macro.

--- debug.h
+++ debug.h
@@ -2,9 +2,11 @@
 #define DEBUG_H
 
 #ifndef NDEBUG
-#define g_here(a) do { fprintf (stderr, "%s:%d", __FILE__, __LINE__); fflush (stderr); } while (0)
+#define g_here(a) do { fprintf (stderr, "%s:%d\n", __FILE__, __LINE__); fflush (stderr); } while (0)
+#define g_debug(...) do { fprintf (stderr, __VA_ARGS__); fflush (stderr); } while (0)
 #else
 #define g_here(a)
+#define g_debug(...)
 #endif
 
 #endif // DEBUG_H
