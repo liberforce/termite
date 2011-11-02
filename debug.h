399908cde9fc9de3349d36e52ed174c341f92b62
Luis Menina <liberforce@freeside.fr>
2011-11-02 15:26:23 +0100

    Fix include file dependency.

--- debug.h
+++ debug.h
@@ -1,6 +1,8 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <stdio.h>
+
 #ifndef NDEBUG
 #define g_here(a) do { fprintf (stderr, "%s:%d\n", __FILE__, __LINE__); fflush (stderr); } while (0)
 #define g_debug(...) do { fprintf (stderr, __VA_ARGS__); fflush (stderr); } while (0)
