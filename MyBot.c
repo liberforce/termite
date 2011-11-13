a6617216226ac8d38161772429433a2946bb29c2
Luis Menina <liberforce@freeside.fr>
2011-11-13 17:09:49 +0100

    Improve time measuring.
    Wall-clock time is measured by gettimeofday.
    We intercept SIGCONT to best evaluate turn beginning.

--- MyBot.c
+++ MyBot.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h> // for sigaction
 
 #include "types.h"
 #include "rules.h"
@@ -8,6 +9,13 @@
 #include "termite.h"
 
 // main, communicates with tournament engine
+
+static State *s = NULL;
+
+void on_signal_continue (int num, siginfo_t *info, void *context)
+{
+	state_timer_reset (s);
+}
 
 int main (int argc, char *argv[]) 
 {
@@ -17,9 +25,17 @@ int main (int argc, char *argv[])
 	gchar *line;
 	Rules rules;
 	State state;
+	s = &state;
 
 	memset (&rules, 0, sizeof (rules));
 	memset (&state, 0, sizeof (state));
+
+	// Intercept SIGCONT to measure turn time
+	struct sigaction newsig;
+	newsig.sa_sigaction = on_signal_continue;
+	sigemptyset (&newsig.sa_mask);
+	newsig.sa_flags = SA_SIGINFO | SA_NODEFER;
+	sigaction (SIGCONT, &newsig, NULL);
 
 	while (game_running) 
 	{
